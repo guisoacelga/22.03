0478ea20f07d86b790f0e291af55494510eaaab7
guisoacelga <guiso.acelga@gmail.com>
2021-03-23 06:29:06 +0100

    Add sumaDiagonalContraria

--- src/FindSmallestTwoDim.java
+++ src/FindSmallestTwoDim.java
@@ -2,10 +2,12 @@ import java.util.Arrays;
 
 public class FindSmallestTwoDim {
     public static void main(String[] args) {
-        int[][] arr2D = {{1,9,0}, {8,5,7}};
+        int[][] arr2D = {{1,9,0,1}, {8,5,1,6}, {4,1,6,7}, {1,1,2,3}};
         int[] indices = findSmallestNr2D(arr2D);
-        System.out.println(Arrays.toString(indices));
-        System.out.println(arr2D[indices[0]][indices[1]]);
+        printArray2D(arr2D);
+        System.out.println("Los indices del nro menor son: " + Arrays.toString(indices));
+        System.out.println("El nro menor es: " + arr2D[indices[0]][indices[1]]);
+        System.out.println("La suma de la diagonal contraria es: " + sumaDiagonalContraria(arr2D));
 
     }
 
@@ -25,5 +27,25 @@ public class FindSmallestTwoDim {
         }
 
         return indices;
+    }
+
+    public static void printArray2D(int arr2D[][]){
+        for (int i = 0; i < arr2D.length; i++) {
+
+            for (int j = 0; j < arr2D[i].length; j++) {
+                System.out.print(arr2D[i][j] + "\t|\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int sumaDiagonalContraria(int[][] arr2D){
+       int suma = 0;
+
+       for (int i = 0; i < arr2D.length; i++) {
+            suma += arr2D[i][arr2D[i].length-(i+1)];
+       }
+
+        return suma;
     }
 }
